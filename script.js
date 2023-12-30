89592c3a8c04e17f7da9a4e18897ed6e99b32fec
arabicps4games <80811378+arabicps4games@users.noreply.github.com>
2023-12-30 14:08:45 +0100

    arabicps4games chatgpt
    arabicps4games chatgpt

---
+++ script.js
@@ -0,0 +1,24 @@
+const chatElement = document.getElementById("chat");
+createIframe();
+  
+function createIframe() {
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://chatgpt.hunet.ai/chat-gpt/Test%20User";
+  iframe.width = "500";
+  iframe.height = "500";
+  iframe.frameBorder ="0";
+  iframe.scrolling = "0";
+  
+  // onload This is ‘ChatGPT’ How can I help you?
+  iframe.onload = function() {
+    this.contentWindow.postMessage({ message: "FOCUS_INPUTBOX" }, "*")
+  }
+
+  chatElement.appendChild(iframe);
+}
+
+function sendMessageToIframe(message) {
+  const chatElement = document.getElementById("chat");
+  const chatIframeElement = chatElement.getElementsByTagName("iframe")[0];
+  chatIframeElement.contentWindow.postMessage({ message }, "*") // Message This is ‘ChatGPT’ How can I help you?
+}
